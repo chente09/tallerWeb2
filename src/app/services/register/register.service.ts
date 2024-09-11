7d4aced409a5e079577767bc35ecc5d123038cae
cabito02 <deluxe02102018@gmail.com>
2024-09-11 18:57:29 -0500

    LoginNuevo

--- src/app/services/register/register.service.ts
+++ src/app/services/register/register.service.ts
@@ -9,17 +9,13 @@ export class RegisterService {
   constructor() { }
 
   createRegister(userData: { email: string, password: string }, additionalData: any): Promise<any> {
-    // Aquí va la lógica para registrar el usuario
     return new Promise((resolve, reject) => {
-      // Simulación de registro exitoso
       resolve({ message: 'Usuario registrado exitosamente' });
     });
   }
 
   createRegisterWithGoogle(): Promise<any> {
-    // Aquí va la lógica para registrar con Google
     return new Promise((resolve, reject) => {
-      // Simulación de registro con Google exitoso
       resolve({ message: 'Usuario registrado con Google exitosamente' });
     });
   }
